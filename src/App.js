6ddac0eddd1a1e581d8e2512b41b9e6b9db72f9e
Vladislav Verbitsky <altrpv@academ-x.ru>
2022-05-23 17:43:07 +0300

    add three func / main TODO logic was added in this commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,129 @@
 import {useState} from 'react'
 import ToDo from "./components/ToDo";
 import ToDoForm from "./components/ToDoForm";
+import TODOabi from './components/TODOabi';
+import Web3 from 'web3';
 
 
 function App() {
 
   const {todos, setTodos} = useState([])
 
-  const addTask = async() => {
-    
+  const connectMetamask = async() => {
   }
+  
+  //contract function
+  const setTask = async(node, chainId, TODOaddress, description) => {
+
+    const provider = await detectEthereumProvider({ 
+			mustBeMetaMask: true 
+		  }) 
+		  if (provider) { 
+			try { 
+				const accounts = await provider.request({ method: 'eth_requestAccounts' });
+				const userAddress = accounts[0]
+
+				const web3 = new Web3(node)
+			
+				const TODOcontract = await new web3.eth.Contract(TODOabi, TODOaddress, {
+					from: userAddress
+				  });
+			
+				const rawTransaction = {
+					//"gasPrice": web3.utils.toHex(gasPriceGwei * 1e9),
+					//"gasLimit": web3.utils.toHex(gasLimit),
+					"to": TODOaddress,
+					"data": await TODOcontract.methods.setTask(description).encodeABI(), 
+					"chainId": chainId
+				  };
+
+				const hash = await provider.request({
+					method: 'eth_sendTransaction',
+					params: [
+						{
+						from: accounts[0],
+					    ...rawTransaction 
+						}
+					],
+				})
+
+        return hash
+        				 
+			} catch(e) { 
+			  console.error (e) 
+			  return false
+			} 
+		  } else { 
+			console.error('Please install MetaMask') 
+			return false
+		  }           
+
+  }
+
+  //contract function
+  const toggleCompleted = async(node, chainId, TODOaddress, ID) => {
+
+    const provider = await detectEthereumProvider({ 
+			mustBeMetaMask: true 
+		  }) 
+		  if (provider) { 
+			try { 
+				const accounts = await provider.request({ method: 'eth_requestAccounts' });
+				const userAddress = accounts[0]
+
+				const web3 = new Web3(this.node)
+			
+				const TODOcontract = await new web3.eth.Contract(TODOabi, TODOaddress, {
+					from: userAddress
+				  });
+			
+				const rawTransaction = {
+					//"gasPrice": web3.utils.toHex(gasPriceGwei * 1e9),
+					//"gasLimit": web3.utils.toHex(gasLimit),
+					"to": TODOaddress,
+					"data": await TODOcontract.methods.toggleCompleted(ID).encodeABI(), 
+					"chainId": chainId
+				  };
+
+				await provider.request({
+					method: 'eth_sendTransaction',
+					params: [
+						{
+						from: accounts[0],
+					    ...rawTransaction 
+						}
+					],
+				  })
+				 
+			} catch(e) { 
+			  console.error (e) 
+			  return false
+			} 
+		  } else { 
+			console.error('Please install MetaMask') 
+			return false
+		  }           
+
+  }
+
+  //contract function
+  const tasks = async(node, chainId, TODOaddress, ID) => {
+
+			const web3 = new Web3(this.node)
+			
+			const TODOcontract = await new web3.eth.Contract(TODOabi, TODOaddress);
+
+      const tasksData = await TODOcontract.methods.tasks(ID).encodeABI()
+			        
+
+  }
+
+
   return (
     <div className="App">
+      <header>
+        <h1>Список задач: </h1>
+      </header>
     </div>
   );
 }
---
+++ src/components/TODOabi.js
@@ -0,0 +1,114 @@
+const TODOabi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_description",
+				"type": "string"
+			}
+		],
+		"name": "setTask",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_description",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "completed",
+				"type": "bool"
+			}
+		],
+		"name": "TaskCompleted",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "completed",
+				"type": "bool"
+			}
+		],
+		"name": "TaskCreated",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "toggleCompleted",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "tasks",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"internalType": "bool",
+				"name": "completed",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+export default TODOabi
